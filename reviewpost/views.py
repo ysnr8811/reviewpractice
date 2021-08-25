a32e8f72d85f12f9e1f8213e0f4b83f9c0024288
ysnr8811 <y.f.arctos9@gmail.com>
2021-08-25 23:01:31 +0900

    ユーザーを登録する処理を追加

--- reviewpost/views.py
+++ reviewpost/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def signupview(request):
     writelog('signup function is called')
-    return render(request, 'signup.html', {'somedata':100})
+    
+    if request.method == 'POST':
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        user = User.objects.create_user(username_data, '', password_data)
+    else:
+        return render(request, 'signup.html', {})
+    return render(request, 'signup.html')
 
 def writelog(text):
     print(text)
