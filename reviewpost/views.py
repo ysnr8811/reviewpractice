ded2e40109187f2795d041332be0d34a8813e8b5
ysnr8811 <y.f.arctos9@gmail.com>
2021-08-25 22:09:22 +0900

    ログの出力

--- reviewpost/views.py
+++ reviewpost/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def signupview(request):
+    writelog('signup function is called')
     return render(request, 'signup.html', {'somedata':100})
+
+def writelog(text):
+    print(text)
