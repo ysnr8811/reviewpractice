907069b3cc56ece07b0d14c53989710b0d9e1f94
ysnr8811 <y.f.arctos9@gmail.com>
2021-08-25 21:49:20 +0900

    urlsの編集

--- reviewproject/urls.py
+++ reviewproject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('reviewpost.urls'))
+    path('', include('reviewpost.urls')),
 ]
