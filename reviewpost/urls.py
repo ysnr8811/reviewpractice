7b50f149fe451d0cb07eb5fd5f5fa66d47f4d152
ysnr8811 <y.f.arctos9@gmail.com>
2021-08-25 21:59:24 +0900

    サインアップ画面の準備

--- reviewpost/urls.py
+++ reviewpost/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.urls import path
+from .views import signupview
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('signup/', signupview, name='signup'),
 ]
--- reviewpost/views.py
+++ reviewpost/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def signupview(request):
+    return render(request, 'signup.html', {'somedata':100})
