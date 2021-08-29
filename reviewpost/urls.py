a00cefd7b247b1d8375fef8fd14ffb71af71b986
ysnr8811 <y.f.arctos9@gmail.com>
2021-08-29 21:19:50 +0900

    cssの設定

--- reviewpost/urls.py
+++ reviewpost/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('signup/', signupview, name='signup'),
     path('login/', loginview, name='login'),
     path('list/', listview, name='list'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-+ static(settings.STATIC_URL, documents_root=settings.STATIC_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
